399e6e1d80332a189133e7bbb895e96b6c3774e0
Michał Witek <m.witek@cctechnology.pl>
2020-07-04 12:44:31 +0200

    load people and starships on app init

--- star-wars-duel/src/app/features/star-wars-data/services/star-wars-data.service.ts
+++ star-wars-duel/src/app/features/star-wars-data/services/star-wars-data.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { StarWarsDataInterface } from '../store/star-wars-data.interface';
+import { loadAllStarships, loadAllPeople } from '../store/star-wars-data.actions';
 
 @Injectable()
 export class StarWarsDataService {
 
-  constructor() { }
+  constructor(private store: Store<StarWarsDataInterface>) {
+  }
+
+  loadAllPeople = () => this.store.dispatch(loadAllPeople());
+  loadAllStarships = () => this.store.dispatch(loadAllStarships());
 }
