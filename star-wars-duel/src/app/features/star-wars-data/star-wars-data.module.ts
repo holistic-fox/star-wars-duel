444f24f72deb94699b24d1c974ee1b59fedc1e90
Michał Witek <m.witek@cctechnology.pl>
2020-07-04 10:31:37 +0200

    register feature store

--- star-wars-duel/src/app/features/star-wars-data/star-wars-data.module.ts
+++ star-wars-duel/src/app/features/star-wars-data/star-wars-data.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StarWarsDataService } from './services/star-wars-data.service';
+import { StoreModule } from '@ngrx/store';
+import { starWarsDataReducer } from './store/star-wars-data.reducer';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature('star_wars_data', starWarsDataReducer)
   ],
   providers: [StarWarsDataService]
 })
