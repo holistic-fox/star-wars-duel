2c62e4a1944feb7dda23177559434bfaab7a01f9
Michał Witek <m.witek@cctechnology.pl>
2020-07-05 10:50:47 +0200

    Delete home page store

--- star-wars-duel/src/app/features/home-page/home-page.module.ts
+++ star-wars-duel/src/app/features/home-page/home-page.module.ts
@@ -1,7 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { homePageReducer } from './store/home-page.reducer';
 import { HomePageComponent } from './containers/home-page/home-page.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -11,7 +9,6 @@ import { AppRoutingModule } from '../../app-routing.module';
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forFeature('home_page', homePageReducer),
     MatCardModule,
     MatGridListModule,
     AppRoutingModule,
